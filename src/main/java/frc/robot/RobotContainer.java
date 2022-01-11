0b2c0eeed449d0328869622d3f172b283b256908
Gregory Kelly <git@gregk.ca>
2022-01-10 21:50:16 -0500

    Migrate code from SDS template; #1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -19,6 +20,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drivetrain m_drivetrain = new Drivetrain(RobotMap.frontLeftModule, RobotMap.frontRightModule, RobotMap.backLeftModule, RobotMap.backRightModule, RobotMap.pigeon);
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,172 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+import com.swervedrivespecialties.swervelib.SwerveModule;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Subsystem representing the swerve drivetrain
+ */
+public class Drivetrain extends SubsystemBase {
+    /**
+     * The left-to-right distance between the drivetrain wheels
+     *
+     * Should be measured from center to center.
+     */
+    public static final double TRACKWIDTH_METERS = 1.0; // FIXME Measure and set trackwidth
+    /**
+     * The front-to-back distance between the drivetrain wheels.
+     *
+     * Should be measured from center to center.
+     */
+    public static final double WHEELBASE_METERS = 1.0; // FIXME Measure and set wheelbase
+
+    /**
+     * The maximum voltage that will be delivered to the drive motors.
+     * <p>
+     * This can be reduced to cap the robot's maximum speed. Typically, this is
+     * useful during initial testing of the robot.
+     */
+    public static final double MAX_VOLTAGE = 12.0;
+
+    // TODO: Measure the drivetrain's maximum velocity or calculate the theoretical.
+    // The formula for calculating the theoretical maximum velocity is:
+    // <Motor free speed RPM> / 60 * <Drive reduction> * <Wheel diameter meters> *
+    // pi
+    // By default this value is setup for a Mk3 standard module using Falcon500s to
+    // drive.
+    // An example of this constant for a Mk4 L2 module with NEOs to drive is:
+    // 5880.0 / 60.0 / SdsModuleConfigurations.MK4_L2.getDriveReduction() *
+    // SdsModuleConfigurations.MK4_L2.getWheelDiameter() * Math.PI
+    /**
+     * The maximum velocity of the robot in meters per second.
+     * <p>
+     * This is a measure of how fast the robot should be able to drive in a straight
+     * line.
+     */
+    public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
+            SdsModuleConfigurations.MK3_STANDARD.getDriveReduction() *
+            SdsModuleConfigurations.MK3_STANDARD.getWheelDiameter() * Math.PI;
+    /**
+     * The maximum angular velocity of the robot in radians per second.
+     * <p>
+     * This is a measure of how fast the robot can rotate in place.
+     */
+    // Here we calculate the theoretical maximum angular velocity. You can also
+    // replace this with a measured amount.
+    public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+            Math.hypot(TRACKWIDTH_METERS / 2.0, WHEELBASE_METERS / 2.0);
+
+    // By default we use a Pigeon for our gyroscope. But if you use another
+    // gyroscope, like a NavX, you can change this.
+    // The important thing about how you configure your gyroscope is that rotating
+    // the robot counter-clockwise should
+    // cause the angle reading to increase until it wraps back over to zero.
+    private final PigeonIMU m_pigeon;
+
+    /**
+     * The model representing the drivetrain's kinematics
+     */
+    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+            // Front left
+            new Translation2d(TRACKWIDTH_METERS / 2.0, WHEELBASE_METERS / 2.0),
+            // Front right
+            new Translation2d(TRACKWIDTH_METERS / 2.0, -WHEELBASE_METERS / 2.0),
+            // Back left
+            new Translation2d(-TRACKWIDTH_METERS / 2.0, WHEELBASE_METERS / 2.0),
+            // Back right
+            new Translation2d(-TRACKWIDTH_METERS / 2.0, -WHEELBASE_METERS / 2.0));
+
+    // These are our modules. We set them in the constructor.
+    private final SwerveModule m_frontLeftModule;
+    private final SwerveModule m_frontRightModule;
+    private final SwerveModule m_backLeftModule;
+    private final SwerveModule m_backRightModule;
+
+    private ChassisSpeeds m_chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+
+    /**
+     * Create a new swerve drivetrain
+     * 
+     * @param frontLeftModule Front-left swerve module
+     * @param frontRightModule Front-right swerve module
+     * @param backLeftModule Back-left swerve module
+     * @param backRightModule Back-right swerve module
+     * @param pigeon Pigeon IMU
+     */
+    public Drivetrain(SwerveModule frontLeftModule, SwerveModule frontRightModule, SwerveModule backLeftModule,
+            SwerveModule backRightModule, PigeonIMU pigeon) {
+        m_frontLeftModule = frontLeftModule;
+        m_frontRightModule = frontRightModule;
+        m_backLeftModule = backLeftModule;
+        m_backRightModule = backRightModule;
+        m_pigeon = pigeon;
+    }
+
+    /**
+     * Sets the gyroscope angle to zero. This can be used to set the direction the
+     * robot is currently facing to the
+     * 'forwards' direction.
+     */
+    public void zeroGyroscope() {
+        // FIXME Remove if you are using a Pigeon
+        m_pigeon.setFusedHeading(0.0);
+
+        // FIXME Uncomment if you are using a NavX
+        // m_navx.zeroYaw();
+    }
+
+    public Rotation2d getGyroscopeRotation() {
+        // FIXME Remove if you are using a Pigeon
+        return Rotation2d.fromDegrees(m_pigeon.getFusedHeading());
+
+        // FIXME Uncomment if you are using a NavX
+        // if (m_navx.isMagnetometerCalibrated()) {
+        // // We will only get valid fused headings if the magnetometer is calibrated
+        // return Rotation2d.fromDegrees(m_navx.getFusedHeading());
+        // }
+        //
+        // // We have to invert the angle of the NavX so that rotating the robot
+        // counter-clockwise makes the angle increase.
+        // return Rotation2d.fromDegrees(360.0 - m_navx.getYaw());
+    }
+
+    /**
+     * Control the drivetrain
+     * 
+     * @param translation X/Y translation, in meters per second
+     * @param rotation Rotation, in radians per second
+     * @param fieldOriented Boolean indicating if directions are field- or robot-oriented
+     */
+    public void drive(Translation2d translation, double rotation, boolean fieldOriented) {
+        rotation *= 2.0 / Math.hypot(WHEELBASE_METERS, TRACKWIDTH_METERS);
+        if (fieldOriented) {
+            m_chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(translation.getX(), translation.getY(), rotation,
+                    Rotation2d.fromDegrees(m_pigeon.getFusedHeading()));
+        } else {
+            m_chassisSpeeds = new ChassisSpeeds(translation.getX(), translation.getY(), rotation);
+        }
+    }
+
+    @Override
+    public void periodic() {
+        SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(m_chassisSpeeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+
+        m_frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                states[0].angle.getRadians());
+        m_frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                states[1].angle.getRadians());
+        m_backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                states[2].angle.getRadians());
+        m_backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                states[3].angle.getRadians());
+    }
+}
