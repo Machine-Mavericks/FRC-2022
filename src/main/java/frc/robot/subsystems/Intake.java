809453553a81038e46e17bddc54856695e9a986d
KittenCat1080ti <71938947+KittenCat180@users.noreply.github.com>
2022-01-31 20:53:31 -0500

    Unfinished slider

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,13 +4,26 @@
 
 package frc.robot.subsystems;
 
+import java.util.Map;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
+
 public class Intake extends SubsystemBase {
+  private NetworkTableEntry m_speed;
+  private NetworkTableEntry m_speedslider;
+
   private TalonFX intakeFalcon = new TalonFX(RobotMap.CANID.INTAKE_FALCON);
   
   public static double MOTORSPEED = 0.8;
@@ -18,11 +31,13 @@ public class Intake extends SubsystemBase {
   /** Creates a new Intake. */
   public Intake() {
     intakeFalcon.set(ControlMode.PercentOutput, 0);
+    initializeShuffleboard();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    updateShuffleboard();
   }
 /**
  * This function will set the falcon motor speed to whatever double is passed in
@@ -30,5 +45,37 @@ public class Intake extends SubsystemBase {
  */
   public void setMotorSpeed(double motorSpeed) {
     intakeFalcon.set(ControlMode.PercentOutput, motorSpeed);
+  }
+
+  public double getMotorSpeed() {
+    return 0;
+  }
+
+    // -------------------- Subsystem Shuffleboard Methods --------------------
+
+  /** Initialize subsystem shuffleboard page and controls */
+  private void initializeShuffleboard() {
+    // Create Intake page in shuffleboard
+    ShuffleboardTab Tab = Shuffleboard.getTab("Intake");
+
+    // create controls to display robot position, angle, and gyro angle
+    ShuffleboardLayout l1 = Tab.getLayout("Intake", BuiltInLayouts.kList);
+    
+    l1.withPosition(0, 0);
+    l1.withSize(1, 4);
+
+    m_speed = l1.add("Speed", 0.0).getEntry();
+    m_speedslider = Tab.add("Speed", MOTORSPEED).withWidget(BuiltInWidgets.kNumberSlider).withProperties(Map.of("min", 0, "max", 1)).getEntry();
+    // m_robotX = l1.add("X (m)", 0.0).getEntry();
+    // m_robotY = l1.add("Y (m)", 0.0).getEntry();
+    // m_robotAngle = l1.add("Angle(deg)", 0.0).getEntry();
+    // m_gyroAngle = l1.add("Gyro(deg)", 0.0).getEntry();
+  }
+
+  /** Update subsystem shuffle board page with current Intake values */
+  private void updateShuffleboard() {
+    // write current intake data
+    m_speed.setDouble(getMotorSpeed());
+    m_speedslider.getDouble(0.0);
   }
 }
