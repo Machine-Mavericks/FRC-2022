92effa0c923c1b52899f5439a1244844e9565d37
bambam7480 <neutrino314@outlook.com>
2022-02-03 20:07:01 -0500

    fixed import statements

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -5,11 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
-import frc.robot.subsystems.Drivetrain;
 import frc.robot.RobotContainer;
 
 
