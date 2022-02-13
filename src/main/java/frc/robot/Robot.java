6778a4b635c852ea9439c33d9fe1d126e1343342
Olivia O'Driscoll <oliviaod9@gmail.com>
2022-02-13 16:55:48 -0500

    added driver station line (commented out)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -63,6 +64,8 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     autonomousCommand = RobotContainer.getAutonomousCommand();
+    
+    // public static DriverStation.Alliance getAlliance() TODO: does not work
 
     // schedule the autonomous command (example)
     if (autonomousCommand != null) {
