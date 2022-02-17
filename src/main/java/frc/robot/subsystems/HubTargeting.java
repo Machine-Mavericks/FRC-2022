208746b27bf37706787a89c85fb83510a587d8d8
lilyen06 <lilyemma06@gmail.com>
2022-02-16 20:50:36 -0500

    Shuffleboard for HubTargeting created

--- src/main/java/frc/robot/subsystems/HubTargeting.java
+++ src/main/java/frc/robot/subsystems/HubTargeting.java
@@ -5,14 +5,25 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 
 public class HubTargeting extends SubsystemBase {
   private Limelight m_hubCamera;
+  private NetworkTableEntry ty;
+  private NetworkTableEntry distance;
+  private NetworkTableEntry RPM;
+  private NetworkTableEntry ready;
 
   /** Creates a new HubTargeting. */
   public HubTargeting() {
     m_hubCamera = new Limelight("hub");
     m_hubCamera.setPipeline(0);
+    initializeShuffleboard();
   }
 
   @Override
@@ -88,6 +99,33 @@ public class HubTargeting extends SubsystemBase {
     double met = EstimateDistance();
     double RPM = 7.5 * met * met - 52.5 * met + 120;
     return RPM;
+  }
+
+  // -------------------- Subsystem Shuffleboard Methods --------------------
+
+  /** Initialize subsystem shuffleboard page and controls */
+  private void initializeShuffleboard() {
+    // Create odometry page in shuffleboard
+    ShuffleboardTab Tab = Shuffleboard.getTab("Hub Target");
+
+    // create controls to display limelight ty, target ready, estimated distance and
+    // estimated RPM
+    ShuffleboardLayout l1 = Tab.getLayout("Hub Target", BuiltInLayouts.kList);
+    l1.withPosition(0, 0);
+    l1.withSize(1, 4);
+    ty = l1.add("limelight ty", 0.0).getEntry();
+    distance = l1.add("Estimated Distance of Target", 0.0).getEntry();
+    RPM = l1.add("Estimated RPM needed for Target", 0.0).getEntry();
+    ready = l1.add("Ready to Shoot", 0.0).getEntry();
+  }
+
+  /** Update subsystem shuffle board page with current targeting values */
+  private void updateShuffleboard() {
+    // write current robot Gyro
+    ty.setDouble(m_hubCamera.getVerticalTargetOffsetAngle());
+    distance.setDouble(EstimateDistance());
+    RPM.setDouble(DistanceRPM());
+    ready.setBoolean(ReadyToShoot());
   }
 
 }
