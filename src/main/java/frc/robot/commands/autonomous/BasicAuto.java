56b78881b750b64bb980775b9c04658cfd386131
gregk27 <git@gregk.ca>
2022-02-20 21:09:56 -0500

    Create basic command group to run simple 2-ball upper hub auto

---
+++ src/main/java/frc/robot/commands/autonomous/BasicAuto.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.SteerTowardsBall;
+import frc.robot.commands.SteerTowardsHub;
+
+/**
+ * Basic auto which starts on right tarmac, grabs nearest ball, and shoots both
+ */
+public class BasicAuto extends SequentialCommandGroup {
+  /** Creates a new BasicAuto. */
+  public BasicAuto() {
+    addCommands(
+      // Intake the ball
+      new SteerTowardsBall(true),
+      // Turn left until hub is in view
+      new TurnToHubCommand(0.5),
+      // Shoot first ball
+      new ParallelCommandGroup(
+        new SteerTowardsHub(),
+        new AutoShootCommand(AutoShootCommand.HIGH_SPEED)
+      ),
+      // Shoot second ball
+      new ParallelCommandGroup(
+        new SteerTowardsHub(),
+        new AutoShootCommand(AutoShootCommand.HIGH_SPEED)
+      ),
+      // Make a dash for ball near station
+      new SteerTowardsBall(true)
+    );
+  }
+}
