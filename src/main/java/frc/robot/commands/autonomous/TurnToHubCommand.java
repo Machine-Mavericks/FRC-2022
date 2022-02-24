209049ded2f4cb853c7ed5231add0dbe26ffcd2c
gregk27 <git@gregk.ca>
2022-02-24 08:24:31 -0500

    Rework turn to hub command to fix issues

--- src/main/java/frc/robot/commands/autonomous/TurnToHubCommand.java
+++ src/main/java/frc/robot/commands/autonomous/TurnToHubCommand.java
@@ -7,6 +7,7 @@ package frc.robot.commands.autonomous;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.Drivetrain;
@@ -16,6 +17,7 @@ import frc.robot.subsystems.Drivetrain;
  */
 public class TurnToHubCommand extends CommandBase {
   double turnSpeed;
+  long timeout;
   /** 
    * Creates a new TurnToHubCommand. 
    * @param turnSpeed Speed to make turn, in %{@link Drivetrain#MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND}
@@ -23,7 +25,8 @@ public class TurnToHubCommand extends CommandBase {
   */
   public TurnToHubCommand(double turnSpeed, double timeout) {
     addRequirements(RobotContainer.drivetrain);
-    this.withTimeout(timeout);
+    this.turnSpeed = turnSpeed;
+    this.timeout = (int) (timeout*1000);
   }
   
   // Called when the command is initially scheduled.
@@ -31,29 +34,25 @@ public class TurnToHubCommand extends CommandBase {
   public void initialize() {
     // Determine direction from odometry
     Pose2d pose = RobotContainer.odometry.getPose2d();
-    double targetAngle = 0;
-    if(pose.getX() < 6.25 && pose.getY() < 3.125) {
-      // If in near left
-      targetAngle = 45;
-    } else if (pose.getX() > 6.25 && pose.getY() < 3.125){
-      // If in far left
-      targetAngle = -45+360;
-    } else if (pose.getX() < 6.25 && pose.getY() > 3.125){
-      // If in near right
-      targetAngle = 135;
-    } else {
-      // If in far right
-      targetAngle = -135+360;
-    }
-    // Get angle, adjust coordiante system to simplify math
-    double currAngle = RobotContainer.odometry.getAngle();
-    if(currAngle < 0) currAngle += 360;
-    // Use sin to determine optimal direction
-    if(Math.sin(currAngle-targetAngle) < 0) {
+    Pose2d hub = new Pose2d(6.25, 3.125, new Rotation2d(0));
+    Pose2d hubDir = pose.relativeTo(hub);
+    
+    double targetAngle = Math.toDegrees(Math.atan2(hubDir.getY(), hubDir.getX()));
+
+    double err = (-pose.getRotation().getDegrees() + targetAngle) % 360;
+    if(Math.abs(err) > 180) err += 360;
+    err %= 360;
+    if(err > 180) err = err-360;
+
+    turnSpeed = Math.abs(turnSpeed);
+    if(err < 0){
       turnSpeed = -turnSpeed;
     }
     // Start driving desired direction
     RobotContainer.drivetrain.drive(new Translation2d(0,0), turnSpeed*Drivetrain.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND, false); 
+
+    // Initialise timeout
+    this.timeout += System.currentTimeMillis();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -70,6 +69,6 @@ public class TurnToHubCommand extends CommandBase {
   @Override
   public boolean isFinished() {
     // End when the hub has been detected
-    return RobotContainer.hubTargeting.IsTarget();
+    return RobotContainer.hubTargeting.IsTarget() || System.currentTimeMillis() > timeout;
   }
 }
