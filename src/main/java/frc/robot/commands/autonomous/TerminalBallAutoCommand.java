95e93f22c62d1639d591fc4fcaaf926af2c595d5
gregk27 <git@gregk.ca>
2022-03-20 20:02:57 -0400

    Get 4-ball auto working

--- src/main/java/frc/robot/commands/autonomous/TerminalBallAutoCommand.java
+++ src/main/java/frc/robot/commands/autonomous/TerminalBallAutoCommand.java
@@ -14,16 +14,19 @@ public class TerminalBallAutoCommand extends SequentialCommandGroup {
     public TerminalBallAutoCommand() {
         addCommands(
             // Make a dash for ball near station
-            new AutoDriveToPose(new Pose2d(new Translation2d(1.5, 1.70), Rotation2d.fromDegrees(-135)), 0.5, 0.25, 30),
+            new AutoDriveToPose(new Pose2d(new Translation2d(1.5, 1.70), Rotation2d.fromDegrees(-135)), 0.75, 0.25, 30),
             new DelayCommand(0.5),
             new SteerTowardsBall(true, 2, 0.2),
+            new DelayCommand(0.5),
+            new SteerTowardsBall(true, 5, 0),
             // Drive back to shooting position
-            new AutoDriveToPose(new Pose2d(new Translation2d(3, 2), Rotation2d.fromDegrees(-135)), 0.25, 0.10, 2),
+            new AutoDriveToPose(new Pose2d(new Translation2d(3, 2), Rotation2d.fromDegrees(-135)), 0.5, 0.10, 2),
             // Turn left until hub is in view
             new TurnToHubCommand(0.20, 2),
-            // // Shoot first ball
+            // Shoot first ball
+            new AutoShootCommand(AutoShootCommand.HIGH_SPEED).deadlineWith(new SteerTowardsHub()),
+            // Shoot second ball
             new AutoShootCommand(AutoShootCommand.HIGH_SPEED).deadlineWith(new SteerTowardsHub())
-
         );
     }
 }
